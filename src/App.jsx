da9eaa045efb6fd1b8c84ec978b16bc27b3d9534
Mike Schwall <mikschwall@gmail.com>
2025-03-28 08:25:13 -0500

    cleanup repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import * as containerStyles from "./App.module.css"
+
+
+function App() {
+
+  return (
+    <div className={containerStyles.container}>
+      test
+    </div>
+  )
+}
+
+export default App
