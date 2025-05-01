33dac58c30e8a5fe991345c7e49cca2ef6e83293
Mike Schwall <mikschwall@gmail.com>
2025-05-01 12:16:15 -0500

    change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // 👈 This makes expect, describe, it globally available
+    environment: 'jsdom',
+    setupFiles: './setupTests.js' // if you use jest-dom
+  },
   build: {
     minify: 'terser', // instead of 'esbuild'
     terserOptions: {
