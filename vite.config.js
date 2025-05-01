c3f48359225565ebe473111c0d83dde335e71646
Mike Schwall <mikschwall@gmail.com>
2025-05-01 12:13:47 -0500

    correct aggressive compression

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.js', // 👈 this is the key
-  },
+  build: {
+    minify: 'terser', // instead of 'esbuild'
+    terserOptions: {
+      compress: {
+        // Avoid aggressive compression
+        drop_console: false,
+        drop_debugger: false,
+        unsafe: false
+      },
+      mangle: false
+    }
+  }
 });
